1503d71381e2c3229d6ab5f0e1663cbfe9db7e98
polashchandra <polashchandra210@gmail.com>
2024-11-13 17:32:08 +0600

    function tuple

---
+++ Typescript/src/FunctionWithGenaris.ts
@@ -0,0 +1,21 @@
+//function with genarics
+{
+    // generic local types
+    const crateArray=(param:string):string[]=>{
+        return [param]
+    }
+    // function generics
+    const crateArraywithgenaric=<T>(param:T):T[]=>{
+        return[param]
+    }
+    const result =crateArray('hello');
+    const result2 =crateArraywithgenaric<boolean>(true)
+    type user3 ={ name:string, age:number }
+    const result3= crateArraywithgenaric<user3>({name:"hello",age:10})
+    //function tuple generics
+    const createTuple=<T,Q>(param:T,param1:Q):[T,Q]=>{
+        return [param,param1]
+    }
+    const result4=createTuple<string,number>('hello',10)
+}
+
