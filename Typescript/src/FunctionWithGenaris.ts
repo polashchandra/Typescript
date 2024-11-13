099aec2cb6495106c70535338ea4b4d6def31b9f
polashchandra <polashchandra210@gmail.com>
2024-11-13 17:52:01 +0600

    functionWithGenaris

--- Typescript/src/FunctionWithGenaris.ts
+++ Typescript/src/FunctionWithGenaris.ts
@@ -8,14 +8,32 @@
     const crateArraywithgenaric=<T>(param:T):T[]=>{
         return[param]
     }
+
     const result =crateArray('hello');
     const result2 =crateArraywithgenaric<boolean>(true)
     type user3 ={ name:string, age:number }
     const result3= crateArraywithgenaric<user3>({name:"hello",age:10})
+
+
     //function tuple generics
     const createTuple=<T,Q>(param:T,param1:Q):[T,Q]=>{
         return [param,param1]
     }
     const result4=createTuple<string,number>('hello',10)
+
+
+
+
+
+    //fixed array generics
+    const fixedGemericArray =<T>(student:T)=>{
+        const couse="programing hero"
+        
+        return{
+            ...student,couse
+        }
+    }
+    const result5 =fixedGemericArray({name:'hello',age:10})
+    
 }
 
