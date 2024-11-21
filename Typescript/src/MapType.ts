cabb87a0081325644a46c0f6ea059bd2418b7766
polashchandra <polashchandra210@gmail.com>
2024-11-21 20:53:41 +0600

    MapTypes digitak dynamic

--- Typescript/src/MapType.ts
+++ Typescript/src/MapType.ts
@@ -1,5 +1,20 @@
-const arrofnumber:number[]=[1,2,3];
+{
+    const arrofnumber:number[]=[1,2,3];
 
-const converttostring:string[]=arrofnumber.map((number)=>number.toString())
-
-console.log(converttostring);
+    const converttostring:string[]=arrofnumber.map((number)=>number.toString())
+    
+    console.log(converttostring);
+    //analog system mapType
+   type ayraNumber={
+        height:number,
+        weight:number,
+    }
+    // type areaType={
+    //     [key in "height"|"weight"]:string
+    // }
+    
+    //digital system mapType
+    type areaTypedynamic={
+        [keys in keyof ayraNumber]:string
+    }
+}
