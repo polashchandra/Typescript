05a58374420d026de687b2dcf014e041a3e11b47
polashchandra <polashchandra210@gmail.com>
2025-01-07 19:57:09 +0600

    oop class and object

---
+++ Objectorientedprograming/ClassAndObject.ts
@@ -0,0 +1,11 @@
+class animal{
+    name:string;
+    species:string;
+    sound:string;
+    constructor(name: string, species: string, sound: string) {
+        this.name = name;
+        this.species = species;
+        this.sound = sound;
+    }
+
+}
--- Typescript/src/Uitility.ts
+++ Typescript/src/Uitility.ts
@@ -8,4 +8,20 @@
     };
 // Pick
     type nameage=Pick<utilities,"name"|"age">;
+
+
+//omit
+
+ type contactInfo = Omit<utilities,"name"|"age">
+ //required
+ type  requiredUtility = Required<utilities>
+
+ //partial
+ type personpartial=Partial<utilities>  
+
+ // Readonly
+ type utlityReadonly = Readonly<utilities>;
+ //Record
+ type myobj=Record<string,unknown>
+
 }
