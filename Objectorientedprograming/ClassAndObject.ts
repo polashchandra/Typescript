4538b2df6bcbac34b444cd46fa55dc5f3c9f44e3
polashchandra <polashchandra210@gmail.com>
2025-01-08 15:49:22 +0600

    this is class object module tutirial

--- Objectorientedprograming/ClassAndObject.ts
+++ Objectorientedprograming/ClassAndObject.ts
@@ -1,11 +1,17 @@
-class animal{
-    name:string;
-    species:string;
-    sound:string;
-    constructor(name: string, species: string, sound: string) {
-        this.name = name;
-        this.species = species;
-        this.sound = sound;
-    }
+{
 
+    class animal{
+        name:string;
+        species:string;
+        sound:string;
+        constructor(name: string, species: string, sound: string) {
+            this.name = name;
+            this.species = species;
+            this.sound = sound;
+        }
+    
+    }
+    const dog= new animal("dog","dog","bark")
+    const lion=new animal("lion","lion","roar")
+    
 }
