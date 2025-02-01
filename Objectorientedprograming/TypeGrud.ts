a4c0e997b3e4b14e57e8a4b3a1ba7575d76b9778
polashchandra <polashchandra210@gmail.com>
2025-02-01 23:44:38 +0600

    this is grud

--- Objectorientedprograming/TypeGrud.ts
+++ Objectorientedprograming/TypeGrud.ts
@@ -11,6 +11,32 @@
     const result=add("2","1")
     console.log(result);
     
+    //in guard
+    type Normal={
+        name:string;
+    }
+    type Admin={
+        name:string;
+        role:string;
+    }
+
+    const getuser=(user:Normal|Admin)=>{
+        if("role" in user){
+            console.log(`my nme is ${user.name} and my role is ${user.role}`);
+            
+
+        }else{
+            console.log(`my name is ${user.name}`);
+            
+        }
+    }
+    const normaluser:Normal={
+        name:"santiyago"
+    }
+    const adminUser:Admin={
+        name:"santiyago",
+        role:"admin"
+    }
 
     
 }
