1d84bbf1c63f3cb4c641ce20d2e16154bef9b5b8
polashchandra <polashchandra210@gmail.com>
2025-02-10 23:00:22 +0600

    this is instances Greads module

---
+++ Objectorientedprograming/InstancesGuard.ts
@@ -0,0 +1,35 @@
+{
+class Animal {
+    name:string;
+    species:string;
+
+    constructor(name:string,species:string){
+        this.name=name;
+        this.species=species;
+    }
+    makesound(){
+        console.log("I am making sound");
+        
+    }
+}
+class dog extends Animal{
+    constructor(name:string,species:string){
+        super(name,species)
+    }
+    makeBrak(){
+        console.log("I am barking");
+    }
+}
+class cat extends Animal{
+    constructor(name:string,species:string){
+        super(name,species)
+    }
+    makeCaw(){
+        console.log('i am cawing');
+    }
+}
+
+const dog=new dog("dog","mammmal");
+const cat =new cat("cat","cawing");
+
+}
