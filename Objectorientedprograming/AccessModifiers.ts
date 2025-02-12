060a145c7d015bb4c34de3daf9341143cb9fde79
polashchandra <polashchandra210@gmail.com>
2025-02-12 15:10:25 +0600

    AccessModifiers.ts file uspload

---
+++ Objectorientedprograming/AccessModifiers.ts
@@ -0,0 +1,30 @@
+{
+    class BankAccount{
+     public readonly id:number;
+     public name:string;
+     protected _balance:number;
+     
+     constructor(id:number,name:string,balance:number){
+        this.id=id;
+        this.name=name;
+        this._balance=balance;
+     }
+     public Adddeposit(amount:number){
+        this._balance=this._balance+amount;
+     }
+     pubic getBalance(){
+        return this._balance;
+     }
+
+    }
+    class studentAccount extends BankAccount{
+        test(){
+            this._balance
+        }
+    }
+
+    const goribmanusherAccount= new BankAccount(111,'goribmanush',1000);
+    goribmanusherAccount.Adddeposit(500);
+    const mybalance=goribmanusherAccount.getBalance();
+    console.log(mybalance);
+}
