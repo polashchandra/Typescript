3e78f630617ceed11a26524e0bf8b62bf44a1005
polashchandra <polashchandra210@gmail.com>
2025-02-17 17:18:42 +0600

    push getter and setter in typescript

---
+++ Objectorientedprograming/GetterAndSetter.ts
@@ -0,0 +1,32 @@
+{
+    // getter and setter
+
+    class BankAccount{
+     public readonly id:number;
+     public name:string;
+     protected _balance:number;
+     
+     constructor(id:number,name:string,balance:number){
+        this.id=id;
+        this.name=name;
+        this._balance=balance;
+     }
+
+     set balance(amount:number){
+        this._balance = this._balance + amount;
+     }
+
+    // Getter
+     get balance(){
+        return this._balance;
+     }
+
+
+    }
+    const goribmanusherAccount= new BankAccount(111,'goribmanush',1000); 
+
+    goribmanusherAccount.balance=500;
+
+    const myBalance=goribmanusherAccount.balance;
+ 
+}
