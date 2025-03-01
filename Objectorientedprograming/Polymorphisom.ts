c9a9a8bbdf6f145f1c93654570735f98441e4602
polashchandra <polashchandra210@gmail.com>
2025-03-01 16:55:26 +0600

    next consept

--- Objectorientedprograming/Polymorphisom.ts
+++ Objectorientedprograming/Polymorphisom.ts
@@ -1,17 +1,25 @@
 {
-    class person{
+    class Person{
         getSleep(){
             console.log("Person is sleeping 8 hours a day");
         }
     }
-    class Student extends person{
+    class Student extends Person{
         getSleep(){
             console.log("Student is sleeping 6 hours a day");
         }
     }
-    class devoloper extends person{
+    class devoloper extends Person{
         getsleep(){
             console.log("devoloper is sleeping 4 hours a day")
         }
     }
+
+    const getsleepingHours=(param:Person)=>{
+        param.getSleep()
+    }
+
+    const person1=new Person();
+    const person2=new Student();
+    const person3=new devoloper();
 }
