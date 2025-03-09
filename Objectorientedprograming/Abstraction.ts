531e93861f9a5fa5d18b0d319e907c09c9d1f708
polashchandra <polashchandra210@gmail.com>
2025-03-10 01:22:22 +0600

    this abstraction topic full completed

--- Objectorientedprograming/Abstraction.ts
+++ Objectorientedprograming/Abstraction.ts
@@ -1,5 +1,6 @@
 {
-    //Anstaction 1.interface 2.abstract
+    //Anstactionআপেসট্রাকশন দুই ভাবে করা যায় ইন্টারফেস এন্ড অ্যাবস্ট্রাক্ট 1.interface 2.abstract
+    //idea
     interface vhicle1{
         starEngine():void;
         stopEngine():void;
@@ -25,5 +26,27 @@
 
     const toyota=new car()
     toyota.starEngine()
+
+    //abstract
+    // idea
+   abstract class car2{
+        abstract starEngine():void;
+        abstract stopEngine():void;
+        abstract move():void;
+    }
+
+    class ToyotaCar extends car2{
+        starEngine(): void {
+            console.log("car started");
+        }
+        stopEngine():void{
+            console.log("car stopped");
+        }
+        move():void{
+            console.log("car moving");
+            
+        }
+    }
+   
 
 }
